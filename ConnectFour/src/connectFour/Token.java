96037e2aea01b62d9d282d35a44b11b1ae0c4eaf
Igor Lopez <igor.lopezs@alumni.mondragon.edu>
2023-09-14 17:47:38 +0200

    Token code

--- ConnectFour/src/connectFour/Token.java
+++ ConnectFour/src/connectFour/Token.java
@@ -2,4 +2,18 @@ package connectFour;
 
 public enum Token {
 
+   NULL, YELLOW, RED;
+
+   public Token get(Integer index) {
+      Token resultToken = null;
+      
+      for (Token token : Token.values()) {
+         if (token.ordinal() == index) {
+            resultToken = token;
+         }
+      }
+
+      return resultToken;
+   }
+
 }
